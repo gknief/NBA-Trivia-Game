5d6d884e8a7e78456346f7cace607b736297ff5b
Grant Knief <gknief@mac.com>
2018-09-06 14:46:22 -0400

    added npm nba library

--- src/App/index.js
+++ src/App/index.js
@@ -10,6 +10,19 @@ class App extends Component {
     }
   }
   
+
+  componentDidMount = async () => {
+    const nbaPlayers = await fetch(`http://api.sportradar.us/nba/trial/v5/en/seasons/2017/REG/leaders.json?api_key=9ckqa4pt9gnte22qkjaafd65
+    `)
+    const nbaPlayersjson = await nbaPlayers.json();
+  
+    console.log(nbaPlayersjson);
+
+}
+
+
+
+
   render() {
     return <div className="App">NBA Trivia</div>;
   }
